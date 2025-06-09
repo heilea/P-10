4691ca34c80af1153d8787656eeefb000d3a560f
Heihachi <heiariim1@gmail.com>
2025-06-09 10:14:17 -1000

    Correction du tri des catégories

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,25 +13,25 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+
+  const filteredEventsByType = type
+    ? data?.events.filter((event) => event.type === type) || []
+    : data?.events || [];
+
+  // Appliquer la pagination sur les événements filtrés
+  const filteredEvents = filteredEventsByType.filter(
+    (_, index) =>
+      (currentPage - 1) * PER_PAGE <= index && PER_PAGE * currentPage > index
+  );
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
